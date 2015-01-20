4103df5c4b0172a4364a4b30ccbd79d851bc95d7
Mithil Arun <arun.mithil@gmail.com>
2015-01-20 14:40:53 +0530

    Changed shim driver to point to generic plugin

--- neutron/plugins/ml2/drivers/pluribus/__init__.py
+++ neutron/plugins/ml2/drivers/pluribus/__init__.py
@@ -1,4 +1,4 @@
-# COPYRIGHT 2014 Pluribus Networks Inc.
+# COPYRIGHT 2015 Pluribus Networks Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,4 +11,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
